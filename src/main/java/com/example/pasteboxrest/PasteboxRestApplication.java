9cfcdfbce2fe2de01043a18242f42f53aa3a369b
Kalips2 <nikitashkola43@gmail.com>
2022-08-12 13:30:16 +0300

    first commit

---
+++ src/main/java/com/example/pasteboxrest/PasteboxRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pasteboxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteboxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteboxRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pasteboxrest/PasteboxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pasteboxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
